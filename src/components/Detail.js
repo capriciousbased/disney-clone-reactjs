cca1b7c01bbc1b829b8379d2d336853b68ef9cdf
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-05-07 13:00:50 +0200

    add Detail page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -11,14 +11,30 @@ const Detail = () => {
         <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78" />
       </ImgTitle>
       <Controls>
-        <PlayButton></PlayButton>
+        <PlayButton>
+          <img src="/images/play-icon-black.png" />
+          <span>PLAY</span>
+        </PlayButton>
 
-        <TrailerButton></TrailerButton>
+        <TrailerButton>
+          <img src="/images/play-icon-white.png" />
+          <span>Trailer</span>
+        </TrailerButton>
 
-        <AddButton></AddButton>
+        <AddButton>
+          <span>+</span>
+        </AddButton>
 
-        <GroupWatchButton></GroupWatchButton>
+        <GroupWatchButton>
+          <img src="/images/group-icon.png" />
+        </GroupWatchButton>
       </Controls>
+      <SubTitle>2018 • 7m • Family, Fantasy, Kids, Animation</SubTitle>
+      <Description>
+        A Chinese mom who’s sad when her grown son leaves home gets another
+        chance at motherhood when one of her dumplings springs to life. But she
+        finds that nothing stays cute and small forever.
+      </Description>
     </Container>
   );
 };
@@ -60,8 +76,66 @@ const ImgTitle = styled.div`
   }
 `;
 
-const Controls = styled.div``;
-const PlayButton = styled.div``;
-const TrailerButton = styled.div``;
-const AddButton = styled.div``;
-const GroupWatchButton = styled.div``;
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const PlayButton = styled.button`
+  border-radius: 4px;
+  font-size: 15px;
+  padding: 0px 24px;
+  margin-right: 22px;
+  display: flex;
+  align-items: center;
+  height: 56px;
+  background: rgb (249, 249, 249);
+  border: none;
+  letter-spacing: 1.8px;
+  cursor: pointer;
+  &:hover {
+    background: rgb(198, 198, 198);
+  }
+`;
+
+const TrailerButton = styled(PlayButton)`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+  text-transform: uppercase;
+`;
+const AddButton = styled.button`
+  margin-right: 16px;
+  width: 44px;
+  height: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  border: 2px solid white;
+  background-color: rgba(0, 0, 0, 0.6);
+  cursor: pointer;
+  span {
+    font-size: 30px;
+    color: white;
+  }
+`;
+
+const GroupWatchButton = styled(AddButton)`
+  background: rgb(0, 0, 0);
+`;
+
+const SubTitle = styled.div`
+  color: rgb(249, 249, 249);
+  font-size: 15px;
+  min-height: 20px;
+  margin-top: 26px;
+`;
+
+const Description = styled.div`
+  line-height: 1.4;
+  font-size: 20px;
+  margin-top: 16px;
+  color: rgb(249, 249, 249);
+  max-width: 760px;
+`;
