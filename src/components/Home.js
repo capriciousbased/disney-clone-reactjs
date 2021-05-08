11646232de54c661ca6d57a13a0a0d100feaca3e
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-05-08 13:34:55 +0200

    firebase added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
 import Movies from "./Movies";
 import Viewers from "./Viewers";
 
 const Home = () => {
+  useEffect(() => {
+    db.collection("movies").onSnapshot((snapshot) => {
+      let tempMovies = snapshot.docs.map((doc) => {
+        return { id: doc.id, ...doc.data() };
+      });
+    });
+  }, []);
+
   return (
     <Container>
       <ImgSlider />
